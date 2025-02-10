a8f8608f091d1c49ee8d04348d4b61e7505786c3
Snow <snowyz3007@gmail.com>
2025-02-10 14:42:20 +0530

    Added arrows to control the testimonials review while keeping the auto chaging system

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ import { review } from './JS/reviews.js';
 let id = 0;
 
 function updateReview() {
+	id = Math.abs(id);
+	if ((id + 1) % 100 === 0) {
+		id = 100;
+	} else {
+		id = (id + 1) % 100;
+	}
+	const curr_review = document.querySelector('.person-review');
+	const curr_worksin = document.querySelector('.worksin');
+	const curr_position = document.querySelector('.position');
+	const curr_name = document.querySelector('.name');
+	const currentReview = review.find((r) => r.id === id);
+	curr_review.innerHTML = `${currentReview.review}`;
+	curr_worksin.innerHTML = `${currentReview.worksin}`;
+	curr_position.innerHTML = `${currentReview.position}`;
+	curr_name.innerHTML = `${currentReview.name}`;
+}
+
+function updateReviewRight() {
+	id = Math.abs(id);
 	if ((id + 1) % 100 === 0) {
 		id = 100;
 	} else {
@@ -19,6 +38,44 @@ function updateReview() {
     curr_name.innerHTML = `${currentReview.name}`;
 }
 
-setInterval(updateReview, 2000);
+function updateReviewLeft() {
+	id = Math.abs(id);
+	if ((id - 1) % 100 === 0) {
+		id = 100;
+	} else {
+		id = (id - 1) % 100;
+	}
+	const curr_review = document.querySelector('.person-review');
+	const curr_worksin = document.querySelector('.worksin');
+	const curr_position = document.querySelector('.position');
+	const curr_name = document.querySelector('.name');
+	const currentReview = review.find((r) => r.id === id);
+	curr_review.innerHTML = `${currentReview.review}`;
+	curr_worksin.innerHTML = `${currentReview.worksin}`;
+	curr_position.innerHTML = `${currentReview.position}`;
+	curr_name.innerHTML = `${currentReview.name}`;
+}
 
-updateReview();
+const leftShift = document.querySelector('.left-arrow');
+
+leftShift.addEventListener('click', () => {
+	updateReviewLeft();
+	clearInterval(Intervalid);
+	autoChangeReview();
+})
+
+const rightShift = document.querySelector('.right-arrow');
+
+rightShift.addEventListener('click', () => {
+	updateReviewRight();
+	clearInterval(Intervalid);
+	autoChangeReview();
+})
+
+let Intervalid;
+
+function autoChangeReview() {
+	Intervalid = setInterval(updateReview, 4000);
+}
+
+autoChangeReview();
